db7b3991cedbc5f7560f0bc1bd3bada867306c84
Siljeo7 <siljekv07@icloud.com>
2025-03-28 10:38:51 +0100

    oppsummering

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ const articles = document.querySelectorAll('article');
 articles.forEach(article => { 
   article.addEventListener('click', show)
 })
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const box = document.querySelector(".oppsum");
+    const button = document.querySelector(".toggle-knapp");
+
+    button.addEventListener("click", function () {
+        box.classList.toggle("utvid");
+
+        button.style.transform = box.classList.contains("utvid") ? "rotate(180deg)" : "rotate(0deg)";
+    });
+});
